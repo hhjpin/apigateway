da3cd1d65d769a7ecb7976beef9611d1ae562618
riposa <riposa@hengha.ren>
2018-07-24 22:22:11 +0800

    sdk

---
+++ sdk/python/const.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+NODE_KEY = "Node"
+NODE_PREFIX = "Node-"
+SERVICE_KEY = "Service"
+SERVICE_PREFIX = "Service-"
+ROUTER_KEY = "Router"
+ROUTER_PREFIX = "Router-"
+HEALTH_CHECK_KEY = "HealthCheck"
+HEALTH_CHECK_PREFIX = "HC-"
+ID_KEY = "ID"
+NAME_KEY = "Name"
+PATH_KEY = "Path"
+TIMEOUT_KEY = "Timeout"
+INTERVAL_KEY = "Interval"
+RETRY_KEY = "Retry"
+RETRY_TIME_KEY = "RetryTime"
--- sdk/python/gw_sdk.py
+++ sdk/python/gw_sdk.py
@@ -1,8 +1,11 @@
 #!/etc/bin/env python
 # -*- coding: utf-8 -*-
 
+import uuid
 import etcd3
 import logging
+
+from .const import *
 
 """
 register config ex:
@@ -28,48 +31,111 @@ register config ex:
     }
 """
 
-class ClientNotExist(Exception): pass
-class PendingRegisterHandlerNotExist(Exception): pass
+
+class ClientNotExist(Exception):
+    pass
+
+
+class PredefinedTypeError(TypeError):
+    pass
+
+
+class NotDefinedAttributeError(Exception):
+    pass
+
+
+class GatewayDefinition(object):
+
+    def __setattr__(self, key, value):
+        if key in self.__dict__:
+            if isinstance(self.__annotations__[key], value):
+                self.__setattr__(key, value)
+            else:
+                raise PredefinedTypeError
+        else:
+            raise NotDefinedAttributeError
+
+
+class ServiceDefinition(GatewayDefinition):
+    id: str
+    name: str
+    node: list
+
+
+class HealthCheckDefinition(GatewayDefinition):
+    id: str
+    path: str
+    timeout: int
+    interval: int
+    retry: bool
+    retry_time: int
+
+
+class NodeDefinition(GatewayDefinition):
+    id: str
+    name: str
+    status: int
+    health_check: str
+    service: str
+
+
+class RouterDefinition(GatewayDefinition):
+    id: str
+    name: str
+    frontend: str
+    backend: str
+    service: str
 
 
 class EtcdConf(object):
+    host: str
+    port: int
+    user: str
+    password: str
 
-    host = str()
-    port = int()
-    user = str()
-    password = str()
+    def __str__(self):
+        return "HOST: {}, PORT: {}, USER: {}, PASSWORD: {}".format(self.host, str(self.port), self.user, self.password)
 
-
-class PendingRouter(object):
-
-
+# class PendingRouter(object):
 
 
 class ApiGatewayRegistrant(object):
 
-    def __init__(self):
+    def __init__(self, service: ServiceDefinition, node: NodeDefinition,
+                 hc: HealthCheckDefinition, etcd_conf: EtcdConf, router: list):
         self._client = None
-        self._r = None
+        self._router = list()
 
-    @classmethod
-    def initialize(cls, etcd_conf: EtcdConf, pending_router: list):
+        uid = uuid.getnode()
+        hc.id = uid
+        node.id = uid
+        node.name += uid
+        node.status = 0
+        service.id = uid
+        service.name += uid
+        self._hc = hc
+        self._node = node
+        self._service = service
+
         ec = etcd_conf
-        self = cls()
         try:
-            self._client = etcd3.client(host=ec.host, port=ec.port, user=ec.user, password=ec.password)
+            self._client = etcd3.client(host=ec.host, port=ec.port, user=ec.user,
+                                        password=ec.password)
         except Exception as e:
             logging.exception(str(e))
             self._client = None
-        self._r = pending_router
-        return self
-
-    def set_health_check(self):
+        for r in router:
+            if not isinstance(r, RouterDefinition):
+                raise PredefinedTypeError
+            self._router.append(r)
 
     def register(self):
 
         if self._client is None:
             raise ClientNotExist
-        if self._r is None:
-            raise PendingRegisterHandlerNotExist
 
-    def _register
+    def _register_node(self):
+        if self._client is None or not isinstance(self._client, etcd3.Etcd3Client):
+            raise ClientNotExist
+
+        self._client.get("")
