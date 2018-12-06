304c47fb3a69a18ec890a881aa81b372c4c2a327
cowboy <cowboy@hengha.ren>
2018-12-06 10:07:39 +0800

    add user auth

--- middleware/auth.go
+++ middleware/auth.go
@@ -5,6 +5,8 @@ import (
 	"github.com/dgrijalva/jwt-go"
 	"github.com/valyala/fasthttp"
 	"strings"
+	"github.com/gomodule/redigo/redis"
+
 )
 
 type CustomClaims struct {
@@ -13,19 +15,48 @@ type CustomClaims struct {
 }
 
 type Auth struct {
+	Redis struct {
+		Addr     string `json:"addr"`
+		Password string `json:"password"`
+		Database string `json:"database"`
+	} `json:"redis"`
 }
 
 const (
 	tokenSignature = "henghajiangiscoming20161010"
+	token2user    = "hhj:token:user"
+
 )
 
 var (
 	authLogger = log.New()
 )
+var redisConn redis.Conn
+
+
+func init() {
+	var auth Auth
+
+	redisConn, err := redis.Dial("tcp", auth.Redis.Addr, redis.DialPassword(auth.Redis.Password))
+	if err != nil {
+		authLogger.Exception(err)
+	}
+	_, err = redisConn.Do("SELECT", auth.Redis.Database)
+	if err != nil {
+		authLogger.Exception(err)
+	}
+}
+
+func GetUserByToken(token string) (userId int, err error) {
+	userId, err = redis.Int(redisConn.Do("HGET", token2user, token))
+	return
+}
+
 
 func (a *Auth) Work(ctx *fasthttp.RequestCtx, errChan chan error) {
 	var token string
 	var authorization string
+
 
 	ctx.Request.Header.VisitAll(func(key, value []byte) {
 		if string(key) == "Authorization" {
@@ -43,21 +74,21 @@ func (a *Auth) Work(ctx *fasthttp.RequestCtx, errChan chan error) {
 	ctx.SetUserValue("user_id", 0)
 
 	if token != "" {
-		_, err := jwt.ParseWithClaims(token, &CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		t, err := jwt.ParseWithClaims(token, &CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
 			return []byte(tokenSignature), nil
 		})
 		if err != nil {
 			authLogger.Exception(err)
 		} else {
-			//hhjConn.Where(&hhj.Tokens{Token: token}).First(&tokens)
-			//if claims, ok := t.Claims.(*CustomClaims); ok && t.Valid {
-			//	if claims.UserID == tokens.UserID {
-			//		// authorization passed
-			//		log.Print(claims.UserID)
-			//		ctx.SetUserValue("is_logged_in", true)
-			//		ctx.SetUserValue("user_id", claims.UserID)
-			//	}
-			//}
+
+			userId, _ := GetUserByToken(token)
+			if claims, ok := t.Claims.(*CustomClaims); ok && t.Valid {
+				if claims.UserID == userId {
+					// authorization passed
+					ctx.SetUserValue("is_logged_in", true)
+					ctx.SetUserValue("user_id", claims.UserID)
+				}
+			}
 		}
 	}
 }
