17aa22b21e52ef5b3b75265ac3cadee4c3be63b0
gecko <gecko@hengha.ren>
2020-07-16 15:19:32 +0800

    整改编译脚本

--- make.sh
+++ make.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
-function check_fail() { if [ $? != 0 ]; then echo -e "\nFail！\n"; exit 1; fi }
-rm api_gateway_v2
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o api_gateway_v2; check_fail
-docker build -t registry.henghajiang.com/hengha/api_gateway_v2:$1 .; check_fail
-docker push registry.henghajiang.com/hengha/api_gateway_v2:$1; check_fail
-rm api_gateway_v2
-echo "Success";
+source ../golang_utils/common.sh
+do_make api_gateway_v2 $1
